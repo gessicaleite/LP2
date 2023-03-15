dd5b60cfc12f71109f438ccf22e4e9ec271d866e
Gessica Araujo <araujo.gessica@gmail.com>
2023-03-15 11:33:37 -0300

    create and customize form

--- Extra/Eventos/Eventos/Form1.Designer.cs
+++ Extra/Eventos/Eventos/Form1.Designer.cs
@@ -28,13 +28,162 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblEndereco = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblCelular = new System.Windows.Forms.Label();
+            this.lblCpf = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnTestar = new System.Windows.Forms.Button();
+            this.mskBxCpf = new System.Windows.Forms.MaskedTextBox();
+            this.mskBxCelular = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(201, 104);
+            this.lblNome.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(79, 29);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "Nome";
+            // 
+            // lblEndereco
+            // 
+            this.lblEndereco.AutoSize = true;
+            this.lblEndereco.Location = new System.Drawing.Point(201, 168);
+            this.lblEndereco.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblEndereco.Name = "lblEndereco";
+            this.lblEndereco.Size = new System.Drawing.Size(118, 29);
+            this.lblEndereco.TabIndex = 1;
+            this.lblEndereco.Text = "Endereço";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(201, 227);
+            this.lblEmail.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(82, 29);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "E-mail";
+            // 
+            // lblCelular
+            // 
+            this.lblCelular.AutoSize = true;
+            this.lblCelular.Location = new System.Drawing.Point(201, 289);
+            this.lblCelular.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblCelular.Name = "lblCelular";
+            this.lblCelular.Size = new System.Drawing.Size(90, 29);
+            this.lblCelular.TabIndex = 3;
+            this.lblCelular.Text = "Celular";
+            // 
+            // lblCpf
+            // 
+            this.lblCpf.AutoSize = true;
+            this.lblCpf.Location = new System.Drawing.Point(201, 355);
+            this.lblCpf.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblCpf.Name = "lblCpf";
+            this.lblCpf.Size = new System.Drawing.Size(61, 29);
+            this.lblCpf.TabIndex = 4;
+            this.lblCpf.Text = "CPF";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(357, 99);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(478, 34);
+            this.txtNome.TabIndex = 10;
+            this.txtNome.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNome_KeyPress);
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Location = new System.Drawing.Point(357, 168);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(478, 34);
+            this.txtEndereco.TabIndex = 11;
+            this.txtEndereco.Validated += new System.EventHandler(this.txtEndereco_Validated);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(357, 227);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(478, 34);
+            this.txtEmail.TabIndex = 12;
+            this.txtEmail.Validating += new System.ComponentModel.CancelEventHandler(this.txtEmail_Validating);
+            // 
+            // btnTestar
+            // 
+            this.btnTestar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnTestar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTestar.ForeColor = System.Drawing.Color.White;
+            this.btnTestar.Location = new System.Drawing.Point(673, 487);
+            this.btnTestar.Name = "btnTestar";
+            this.btnTestar.Size = new System.Drawing.Size(162, 87);
+            this.btnTestar.TabIndex = 15;
+            this.btnTestar.Text = "Testar";
+            this.btnTestar.UseVisualStyleBackColor = false;
+            // 
+            // mskBxCpf
+            // 
+            this.mskBxCpf.Location = new System.Drawing.Point(357, 350);
+            this.mskBxCpf.Mask = "000,000,000-00";
+            this.mskBxCpf.Name = "mskBxCpf";
+            this.mskBxCpf.Size = new System.Drawing.Size(170, 34);
+            this.mskBxCpf.TabIndex = 14;
+            this.mskBxCpf.TextMaskFormat = System.Windows.Forms.MaskFormat.IncludePromptAndLiterals;
+            this.mskBxCpf.Enter += new System.EventHandler(this.mskBxCpf_Enter);
+            // 
+            // mskBxCelular
+            // 
+            this.mskBxCelular.Location = new System.Drawing.Point(357, 286);
+            this.mskBxCelular.Mask = "(00)90000-0000";
+            this.mskBxCelular.Name = "mskBxCelular";
+            this.mskBxCelular.Size = new System.Drawing.Size(180, 34);
+            this.mskBxCelular.TabIndex = 13;
+            this.mskBxCelular.Leave += new System.EventHandler(this.mskBxCelular_Leave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1082, 663);
+            this.Controls.Add(this.mskBxCelular);
+            this.Controls.Add(this.mskBxCpf);
+            this.Controls.Add(this.btnTestar);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblCpf);
+            this.Controls.Add(this.lblCelular);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblEndereco);
+            this.Controls.Add(this.lblNome);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Cadastro de Aluno";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblEndereco;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblCelular;
+        private System.Windows.Forms.Label lblCpf;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnTestar;
+        private System.Windows.Forms.MaskedTextBox mskBxCpf;
+        private System.Windows.Forms.MaskedTextBox mskBxCelular;
     }
 }
 
