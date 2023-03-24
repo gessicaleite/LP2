2da3513b903549bf0eafff445b931eaa9bc4b636
GESSICA RAFAELA PINTO DE ARAUJO LEITE <0030482313028@lti.fatecso.edu.br>
2023-03-24 20:32:40 -0300

    add calculadoraVolume

---
+++ Atividades/Atividade1/CalculadoraVolume/CalculadoraVolume/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace CalculadoraVolume
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtRaio = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.lblRaio = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.BackColor = System.Drawing.SystemColors.Control;
+            this.txtAltura.Location = new System.Drawing.Point(252, 152);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(132, 20);
+            this.txtAltura.TabIndex = 2;
+            this.txtAltura.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtAltura_KeyPress);
+            this.txtAltura.Validated += new System.EventHandler(this.TxtAltura_Validated);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtRaio
+            // 
+            this.txtRaio.BackColor = System.Drawing.SystemColors.Control;
+            this.txtRaio.Location = new System.Drawing.Point(252, 89);
+            this.txtRaio.Name = "txtRaio";
+            this.txtRaio.Size = new System.Drawing.Size(132, 20);
+            this.txtRaio.TabIndex = 1;
+            this.txtRaio.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtRaio_KeyPress);
+            this.txtRaio.Validated += new System.EventHandler(this.TxtRaio_Validated);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.BackColor = System.Drawing.SystemColors.Control;
+            this.txtResultado.Enabled = false;
+            this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(252, 212);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(132, 23);
+            this.txtResultado.TabIndex = 4;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnCalcular.Font = new System.Drawing.Font("Marlett", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.ForeColor = System.Drawing.Color.White;
+            this.btnCalcular.Location = new System.Drawing.Point(158, 324);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(103, 61);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.BtnCalcular_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.Red;
+            this.btnSair.Font = new System.Drawing.Font("Marlett", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.ForeColor = System.Drawing.Color.White;
+            this.btnSair.Location = new System.Drawing.Point(378, 324);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(103, 61);
+            this.btnSair.TabIndex = 5;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.BtnSair_Click);
+            // 
+            // lblRaio
+            // 
+            this.lblRaio.AutoSize = true;
+            this.lblRaio.Font = new System.Drawing.Font("Marlett", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRaio.Location = new System.Drawing.Point(155, 89);
+            this.lblRaio.Name = "lblRaio";
+            this.lblRaio.Size = new System.Drawing.Size(37, 16);
+            this.lblRaio.TabIndex = 6;
+            this.lblRaio.Text = "Raio";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Font = new System.Drawing.Font("Marlett", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAltura.Location = new System.Drawing.Point(155, 156);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(45, 16);
+            this.lblAltura.TabIndex = 7;
+            this.lblAltura.Text = "Altura";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Marlett", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(155, 219);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(72, 16);
+            this.lblResultado.TabIndex = 8;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(610, 450);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblRaio);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtRaio);
+            this.Controls.Add(this.txtAltura);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtRaio;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label lblRaio;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ Atividades/Atividade1/CalculadoraVolume/CalculadoraVolume/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CalculadoraVolume
+{
+    public partial class Form1 : Form
+    {
+        double raio, altura, volume;
+        const string mensagem = "Digite um valor válido!";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TxtRaio_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            ChangeEnterToTab(e);
+        }
+
+        private void TxtAltura_Validated(object sender, EventArgs e)
+        {
+            if (!double.TryParse(txtRaio.Text, out altura))
+            {
+                MessageBox.Show(mensagem);
+            }
+            if (altura <= 0)
+            {
+                MessageBox.Show("Altura não pode ser menor que 1");
+            }
+        }
+
+        private void BtnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void BtnCalcular_Click(object sender, EventArgs e)
+        {
+            if(!double.TryParse(txtAltura.Text, out altura) ||
+                !double.TryParse(txtRaio.Text, out raio))
+            {
+                MessageBox.Show("Digite valores válidos!");
+            }
+            else
+            {
+                if(altura<=0 || raio <= 0)
+                {
+                    MessageBox.Show("Valores precisam ser maior que 0");
+                }
+                else
+                {
+                    volume = Math.PI * Math.Pow(raio, 2) * altura;
+                    txtResultado.Text = volume.ToString("N2");
+                }
+            }
+        }
+
+
+        private void TxtAltura_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            ChangeEnterToTab(e);
+        }
+
+        private void TxtRaio_Validated(object sender, EventArgs e)
+        {
+            if (!double.TryParse(txtRaio.Text, out raio))
+            {
+                MessageBox.Show(mensagem);
+            }
+        }
+
+        private void ChangeEnterToTab(KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                SendKeys.Send("{TAB}");
+                e.Handled = true;
+            }
+        }
+    }
+}
